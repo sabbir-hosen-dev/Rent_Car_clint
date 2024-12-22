7cd0d0a1c8c25c1aa56dd394cbfd36d31bc06a64
sabbir-hosen-dev <tssabbirhosen@gmail.com>
2024-12-23 03:20:34 +0600

    page not found

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -1,11 +1,23 @@
-
+import { Link } from "react-router-dom";
 
 function NotFound() {
   return (
-    <div>
-      page not fount
+    <div className="w-full h-screen flex justify-center items-center md:flex-row flex-col">
+      <div className="flex  ">
+        <img
+          className="w-[350px] m-auto"
+          src="https://assets.dochipo.com/editor/animations/404-error/36342188-38b5-4d0e-b134-e713b9027592.gif"
+          alt=""
+        />
+      </div>
+      <div className="flex flex-col gap-5 ">
+        <h1 className="m-auto font-bold text-3xl text-primary ">
+          Page Not Found
+        </h1>
+        <Link to="/" className="my-btn inline-block m-auto ">GO HOME</Link>
+      </div>
     </div>
-  )
+  );
 }
 
-export default NotFound
+export default NotFound;
